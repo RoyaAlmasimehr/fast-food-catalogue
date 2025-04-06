bfb3e0bba98cafc48bd98b93818595f9e85e2dfb
RoyaAlmasimehr <almasimehrroya@gmail.com>
2025-04-06 16:46:14 +0330

    add searchBar

--- fast-food/src/CategoryList/categoryList.jsx
+++ fast-food/src/CategoryList/categoryList.jsx
@@ -1,50 +1,52 @@
 import { useEffect, useState } from "react";
-import axios from "../axios";
 import Loading from "../Loading/loading";
+import SearchBar from "../SearchBar/SearchBar";
+import useAxios from "../useAxios";
 
-const CategoryList = () => {
-const [loading ,setLoading]=useState(true)
-const [categories ,setCategories]=useState([])
+const CategoryList = ({ filterItems, children }) => {
+  const [categories, , loading] = useAxios({
+    url: "/FoodCategory/categories",
+  });
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const response = await axios.get("/FoodCategory/categories");
-    setCategories(response.data);
-    setLoading(false)
-    };
-    fetchCategories()
-  }, []);
-
-  const renderContent =()=>{
-    if(loading){
-      return<Loading />
+  const renderContent = () => {
+    if (loading) {
+      return <Loading theme="primary" />;
     }
-   
-      return (
+
+    return (
+      <div className="ps-3 w-100 d-flex align-items-center justify-content-between gap-5">
         <ul className="nav">
-          <li className="nav-item">
+          <li className="nav-item" onClick={() => filterItems()}>
             <a className="nav-link" href="#">
-              {" "}
-              همه فست فود ها
+              همه فست فودها
             </a>
           </li>
           {categories.map((category) => (
-            <li className="nav-item " key={category.id}>
+            <li
+              className="nav-item"
+              key={category.id}
+              onClick={() => filterItems(category.id)}
+            >
               <a className="nav-link" href="#">
                 {category.name}
               </a>
             </li>
           ))}
         </ul>
-      );
-  
-  }
+        {children}
+      </div>
+    );
+  };
+
   return (
-  <nav className="container mt-n5">
-    <div className=" d-flex align-items-center   bg-white  rounded-3 shadow-lg py-4" style={{height:"80px"}}>
-{renderContent()}
-    </div>
-  </nav>
+    <nav className="container mt-n5">
+      <div
+        className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
+        style={{ height: "80px" }}
+      >
+        {renderContent()}
+      </div>
+    </nav>
   );
 };
 
